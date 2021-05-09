76aa794547ce15b3d95fb5183af004d179fbfd23
Eugene <Eugeniolio@yandex.com>
2021-05-09 16:05:06 +0300

    Initial Commit

---
+++ DiceSwiftUI/DiceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceSwiftUIApp.swift
+//  DiceSwiftUI
+//
+//  Created by Евгений Фирман on 09.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct DiceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
